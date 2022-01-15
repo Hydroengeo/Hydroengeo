a2b1fe79cc84720dcdc1e7665cdb1a3a8f71b8e5
jakhongirov <87989795+jakhongirov@users.noreply.github.com>
2022-01-15 23:40:22 +0500

    add 4 pages

---
+++ src/components/education/education.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+import firstImg from '../../assets/images/education1.jpg'
+import secondImg from '../../assets/images/education2.jpg'
+
+function education() {
+   return (
+      <section className="education">
+         <div className="container">
+            <div className="education__info-box">
+               <h2 className="education__info-box__heading">Образование</h2>
+
+               <p className="education__info-box__info">
+                  Каждый квартал, согласно утвержденного плана в институте организованы курсы повышения квалификации <br />
+                  специалистов по следующим направлениям:
+               </p>
+
+               <p className="education__info-box__info">
+                  графические пакеты семейств Corel DRAW и Adobe Photoshop; <br />
+                  геоинформационные графические базы данных — MapInfo и ArcGIS 10.3<br />
+                  По завершению выдаются дипломы об окончании соответствующих курсов.<br />
+               </p>
+
+               <p className="education__info-box__info education__info-box__info--margin">
+                  Курсы проходят в специализированных классах, группами до 10 человек в виде теоретических и <br />
+                  практических занятий. На каждого учащегося компьютер предоставляется персонально. При проведении <br />
+                  курсов используются презентационные материалы, обучающие программы.
+               </p>
+
+               <div className="education__img-box">
+                  <Image className="education__img-box__img" src={secondImg} alt="education" width='540' height='360' />
+                  <Image className="education__img-box__img" src={firstImg} alt="education" width='540' height='360' />
+               </div>
+            </div>
+         </div>
+      </section>
+   )
+}
+
+export default education
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -14,6 +14,11 @@ import Blazon from "../../assets/images/blazon.png";
 import Arrow from "../../assets/images/downarrow.png";
 import Open from "../../assets/images/open.png";
 import Close from "../../assets/images/close.png";
+import facebook from "../../assets/images/facebook.svg";
+import twitter from "../../assets/images/twitter.svg";
+import instagram from "../../assets/images/instagram.svg";
+import youtube from "../../assets/images/youtube.svg";
+import telegram from "../../assets/images/telegram.svg";
 
 const Layout = ({ children }) => {
   const dropdwn1 = useRef(null);
@@ -315,9 +320,8 @@ const Layout = ({ children }) => {
 
                   <ul ref={dropdwn3} className="navbar-item__dropdown  dropdown3--active">
                     <li
-                      className={`navbar__dropdown-item ${
-                        activeClass1 ? "navbar__dropdown-item--active" : ""
-                      }`}
+                      className={`navbar__dropdown-item ${activeClass1 ? "navbar__dropdown-item--active" : ""
+                        }`}
                     >
                       <button
                         className="navbar__dropdown-link"
@@ -338,9 +342,8 @@ const Layout = ({ children }) => {
                     </li>
 
                     <li
-                      className={`navbar__dropdown-item ${
-                        activeClass2 ? "navbar__dropdown-item--active" : ""
-                      }`}
+                      className={`navbar__dropdown-item ${activeClass2 ? "navbar__dropdown-item--active" : ""
+                        }`}
                     >
                       <button
                         className="navbar__dropdown-link "
@@ -364,9 +367,8 @@ const Layout = ({ children }) => {
                     </li>
 
                     <li
-                      className={`navbar__dropdown-item ${
-                        activeClass3 ? "navbar__dropdown-item--active" : ""
-                      }`}
+                      className={`navbar__dropdown-item ${activeClass3 ? "navbar__dropdown-item--active" : ""
+                        }`}
                     >
                       <button
                         className="navbar__dropdown-link"
@@ -437,7 +439,298 @@ const Layout = ({ children }) => {
         </div>
       </header>
       <main>{children}</main>
-      <footer id="footer"></footer>
+
+      {/* //Footer */}
+      <footer className="footer" id="footer">
+        <div className="border-box"></div>
+        <div className="container">
+          <div className="footer__left-box">
+            <ul className="left-box__list-top">
+              <li className="left-box__item-top">
+                <div className="logo-box">
+                  <Link href="/">
+                    <a className="header__logo">
+                      <Image src={Logo} alt="Hydroengo logo" width="295" height="120" />
+                    </a>
+                  </Link>
+                </div>
+
+                <div className="app-box">
+                  <ul className="app-box__list">
+                    <li className="app-box__item">
+                      <a className="app-box__link" href="https://m.facebook.com/login/?locale2=ru_RU">
+                        <Image src={facebook} alt="facebook logo" width="36" height="36" />
+                      </a>
+                    </li>
+
+                    <li className="app-box__item">
+                      <a className="app-box__link" href="https://twitter.com/login">
+                        <Image src={twitter} alt="twitter logo" width="36" height="36" />
+                      </a>
+                    </li>
+
+                    <li className="app-box__item">
+                      <a className="app-box__link" href="https://www.instagram.com/">
+                        <Image src={instagram} alt="instagram logo" width="36" height="36" />
+                      </a>
+                    </li>
+
+
+                    <li className="app-box__item">
+                      <a className="app-box__link" href="https://www.youtube.com/?hl=ru">
+                        <Image src={youtube} alt="youtube logo" width="36" height="36" />
+                      </a>
+                    </li>
+
+                    <li className="app-box__item">
+                      <a className="app-box__link" href="https://telegram.org/">
+                        <Image src={telegram} alt="telegram logo" width="36" height="36" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                {/* Disply-block 890px */}
+                <ul className="left-box__list-bottom left-box__list-bottom--display-2">
+                  <li className="left-box__item-bottom">
+                    <a className="left-box__item-bottom__link"
+                      href="https://www.google.ru/maps/search/100041+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD,+%D0%A2%D0%B0%D1%88%D0%BA%D0%B5%D0%BD%D1%82+%D1%83%D0%BB.+%D0%9E%D0%BB%D0%B8%D0%BC%D0%BB%D0%B0%D1%80+-64/@41.2825125,69.1392826,11z/data=!3m1!4b1"
+                    >
+                      100041 Узбекистан, Ташкент <br />
+                      ул. Олимлар -64
+                    </a>
+                  </li>
+
+                  <li className="left-box__item-bottom">
+                    <a className="left-box__item-bottom__link" href="https://hydroengeo.uz/">
+                      hydrouz@hydroengeo.uz <br />
+                      hydrouz@exat.uz
+                    </a>
+                  </li>
+
+                  <li className="left-box__item-bottom">
+                    <a className="left-box__item-bottom__link" href="tel: +998712650378">
+                      Tel: 998(71)265-03-78
+                    </a>
+                  </li>
+                </ul>
+
+              </li>
+              <li className="left-box__item-top">
+                <Link href="/">
+                  <a className="left-box__item-top__title">
+                    <strong>ОБ ИНСТИТУТЕ</strong>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>СТРУКТУРА ИНСТИТУТА</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>РУКОВОДСТВО</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>ИСТОРИЯ ИНСТИТУТА</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>МИССИЯ И ПОЛИТИКА</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>СОТРУДНИЧЕСТВО</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>ИСТОРИЯ И РУКОВОДСТВО</p>
+                  </a>
+                </Link>
+              </li>
+
+              <li className="left-box__item-top">
+                <Link href="/">
+                  <a className="left-box__item-top__title">
+                    <strong>ДЕЯТЕЛЬНОСТЬ</strong>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>НАУЧНАЯ</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>ПРОИЗВОДСТВЕННАЯ</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>ОБРАЗОВАНИЕ</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__title">
+                    <strong>НОРМАТИВНАЯ БАЗА</strong>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>КОНТАКТЫ</p>
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="left-box__item-top__info">
+                    <p>ИНТЕРАКТИВНЫЕ УСЛУГИ</p>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="left-box__list-bottom">
+              <li className="left-box__item-bottom">
+                <a className="left-box__item-bottom__link"
+                  href="https://www.google.ru/maps/search/100041+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD,+%D0%A2%D0%B0%D1%88%D0%BA%D0%B5%D0%BD%D1%82+%D1%83%D0%BB.+%D0%9E%D0%BB%D0%B8%D0%BC%D0%BB%D0%B0%D1%80+-64/@41.2825125,69.1392826,11z/data=!3m1!4b1"
+                >
+                  100041 Узбекистан, Ташкент <br />
+                  ул. Олимлар -64
+                </a>
+              </li>
+
+              <li className="left-box__item-bottom">
+                <a className="left-box__item-bottom__link" href="https://hydroengeo.uz/">
+                  hydrouz@hydroengeo.uz <br />
+                  hydrouz@exat.uz
+                </a>
+              </li>
+
+              <li className="left-box__item-bottom">
+                <a className="left-box__item-bottom__link" href="tel: +998712650378">
+                  Tel: 998(71)265-03-78
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__right-box">
+            <Link href="/">
+              <a className="left-box__item-top__title">
+                <strong>ЛАБОРАТОРИИ ИНСТИТУТА</strong>
+              </a>
+            </Link>
+
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>МЕЛИОРАТИВНОЙ ГИДРОГЕОЛОГИИ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>ИНЖЕНЕРНОЙ ГЕОДИНАМИКИ И ДИСТАНЦИОННОГО ЗОНДИРОВАНИЯ ЗЕМЛИ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>ГИДРОГЕОЛОГИИ И ИНЖЕНЕРНОЙ ГЕОЛОГИИ МЕСТОРОЖДЕНИЙ ТВЕРДЫХ
+                  ПОЛЕЗНЫХ ИСКОПАЕМЫХ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>ГЕОИНФОРМАЦИОННЫХ ТЕХНОЛОГИЙ И МОДЕЛИРОВАНИЯ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>ГЕОЭКОЛОГИИ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>ГИДРОГЕОФИЗИКИ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>МОНИТОРИНГА ПОДЗЕМНЫХ ВОД И РЕГИОНАЛЬНЫХ ИССЛЕДОВАНИЙ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>РЕСУРСОВ ПОДЗЕМНЫХ ВОД</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>УРАНА И ГИДРОМИНЕРАЛЬНЫХ РЕСУРСОВ</p>
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>Гидрогеология и инженерная геология рудных месторождений</p>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="left-box__item-top__info">
+                <p>Геоэколого-химическая лаборатория</p>
+              </a>
+            </Link>
+          </div>
+
+          {/* Display-flex 1225*/}
+          <ul className="left-box__list-bottom left-box__list-bottom--display">
+            <li className="left-box__item-bottom">
+              <a className="left-box__item-bottom__link"
+                href="https://www.google.ru/maps/search/100041+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD,+%D0%A2%D0%B0%D1%88%D0%BA%D0%B5%D0%BD%D1%82+%D1%83%D0%BB.+%D0%9E%D0%BB%D0%B8%D0%BC%D0%BB%D0%B0%D1%80+-64/@41.2825125,69.1392826,11z/data=!3m1!4b1"
+              >
+                100041 Узбекистан, Ташкент <br />
+                ул. Олимлар -64
+              </a>
+            </li>
+
+            <li className="left-box__item-bottom">
+              <a className="left-box__item-bottom__link" href="https://hydroengeo.uz/">
+                hydrouz@hydroengeo.uz <br />
+                hydrouz@exat.uz
+              </a>
+            </li>
+
+            <li className="left-box__item-bottom">
+              <a className="left-box__item-bottom__link" href="tel: +998712650378">
+                Tel: 998(71)265-03-78
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 };
