04af84dd2c13edda127763b06dd9fa7059e53fb6
jakhongirov <87989795+jakhongirov@users.noreply.github.com>
2022-01-15 16:42:39 +0500

    add

---
+++ src/components/production/production.js
@@ -0,0 +1,48 @@
+function production() {
+   return (
+      <section className="production">
+         <div className="container">
+            <div className="production__info-box">
+               <h2 className="production__info-box__heading">Производственная деятельность</h2>
+
+               <p className="production__info-box__info">Институт осуществляет:</p>
+
+               <ul className="production__info-box__list">
+                  <li className="production__info-box__item">изучение и использование водных ресурсов;</li>
+                  <li className="production__info-box__item">изучение и использование гидроминеральных ресурсов;</li>
+                  <li className="production__info-box__item">исследования в области мелиоративной гидрогеологии;</li>
+                  <li className="production__info-box__item">мониторинг подземных вод;</li>
+                  <li className="production__info-box__item">изучение процессов загрязнения и истощения запасов вод;</li>
+                  <li className="production__info-box__item">исследования в области инженерной геодинамики;</li>
+                  <li className="production__info-box__item">изучение гидрогеологических и инженерно-геологических условий месторождений полезных ископаемых;</li>
+                  <li className="production__info-box__item">изучение экологических условий городов и промышленных зон;</li>
+                  <li className="production__info-box__item">разработку новых технических средств;</li>
+                  <li className="production__info-box__item">изучение экологических условий городов и промышленных зон;</li>
+                  <li className="production__info-box__item">разработку новых технических средств;</li>
+                  <li className="production__info-box__item">разработку технологий разведки подземных вод и их рациональное использование;</li>
+                  <li className="production__info-box__item">разработку технологий разведки подземных вод и их рациональное использование;</li>
+                  <li className="production__info-box__item">разработку технологий искусственного пополнения запасов пресных подземных вод и защита их от загрязнения;</li>
+                  <li className="production__info-box__item">разработку технологий переработки гидроминерального сырья и их использование;</li>
+                  <li className="production__info-box__item">определение физико-механических свойств горных пород;</li>
+                  <li className="production__info-box__item">ведение топогеодезических работ и выпуск карт;</li>
+                  <li className="production__info-box__item">строительство и капитальный ремонт;</li>
+                  <li className="production__info-box__item">оказание платных услуг юридическим и физическим лицам;</li>
+                  <li className="production__info-box__item">производство удобрений для сельского хозяйства;</li>
+                  <li className="production__info-box__item">выращивание сельскохозяйственных продуктов;</li>
+                  <li className="production__info-box__item">розлив минеральных и питьевых вод;</li>
+                  <li className="production__info-box__item">издание монографий, сборников, рекомендаций, инструкций, статей, рекламных статей и других публикаций;</li>
+                  <li className="production__info-box__item">сотрудничество с организациями, учреждениями, предприятиями и ведомствами Узбекистана и зарубежных стран;</li>
+                  <li className="production__info-box__item">подготовка кадров научных сотрудников через докторантуру, аспирантуру и соискательство;</li>
+                  <li className="production__info-box__item">обучение на курсах повышения квалификации;</li>
+                  <li className="production__info-box__item">открытие филиалов учебных кафедр и обучения студентов ВУЗов.</li>
+               </ul>
+
+               <p className="production__info-box__info">Отдельными видами деятельности Институт может заниматься на основании лицензий</p>
+            </div>
+         </div>
+      </section>
+   )
+
+}
+
+export default production
